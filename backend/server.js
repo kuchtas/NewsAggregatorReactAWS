eba576cdda7863ed20584e2b86293483d97aefe6
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-07-14 18:13:27 +0200

    Connect front with backend for development

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { getWPROST } = require("./getWPROST");
 const { getDZIENNIK } = require("./getDZIENNIK");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
 

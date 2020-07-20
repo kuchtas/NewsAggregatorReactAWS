aa8059fbc31fa6aff69ed99be922eb3d83330f4e
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-07-20 11:52:34 +0200

    Move Search state up to Body component

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search";
 import News from "./News";
 
 const Body = () => {
+  const [searchString, setSearchString] = useState("");
+
+  const sendSearchString = async () => {
+    fetch(
+      `https://07fj9bjzr9.execute-api.eu-central-1.amazonaws.com/default/acquireNews`,
+      {
+        method: "POST",
+        withCredentials: true,
+        headers: {
+          "X-Api-Key": process.env.REACT_APP_API_KEY,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ searchString: searchString }),
+      }
+    )
+      .then((resp) => resp.json())
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (searchString.length !== 0) {
+      sendSearchString();
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearchString(event.target.value);
+  };
   return (
     <div className="container">
-      <Search />
+      <Search onSubmit={handleSubmit} onChange={handleChange} />
       <News />
     </div>
   );
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,49 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Search = () => {
-  const [searchString, setSearchString] = useState("");
-
-  const sendSearchString = async () => {
-    fetch(
-      `https://07fj9bjzr9.execute-api.eu-central-1.amazonaws.com/default/acquireNews`,
-      {
-        method: "POST",
-        withCredentials: true,
-        headers: {
-          "X-Api-Key": process.env.REACT_APP_API_KEY,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ searchString: searchString }),
-      }
-    )
-      .then((resp) => resp.json())
-      .then(function (data) {
-        console.log(data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (searchString.length !== 0) {
-      sendSearchString();
-    }
-  };
-
-  const handleChange = (event) => {
-    setSearchString(event.target.value);
-  };
-
+const Search = ({ onSubmit, onChange }) => {
   return (
     <div className="justify-content-center mt-3">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={onSubmit}>
         <input
           className="form-control"
           type="text"
           placeholder="Na jaki temat chcesz poszukać wiadomości?"
-          onChange={handleChange}
+          onChange={onChange}
         />
       </form>
     </div>

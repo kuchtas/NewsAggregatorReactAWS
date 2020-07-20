7bb88957db1b275e42ff8aeab6acbc3fdddcf1be
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-07-20 17:55:01 +0200

    Add article representation on site

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import { Container } from "reactstrap";
+import wprostLogo from "../logos/wprost-favicon-512x512.png";
+import missingPicture from "../logos/icons8-unavailable-120.png";
 
 const Article = ({ site, title, link, thumbnail }) => {
+  const getLogo = (site) => {
+    switch (site) {
+      case "WPROST":
+        return wprostLogo;
+    }
+  };
+
   return (
-    <div className="container">
-      {site}, {title}, {link}, {thumbnail}
-    </div>
+    <Container className="mt-3 border" style={{ position: "relative" }}>
+      <img
+        src={getLogo(site)}
+        width="50"
+        height="50"
+        alt="Logo of the news site"
+      />
+      <img
+        src={thumbnail}
+        onError={(e) => (e.target.src = missingPicture)}
+        width="100"
+        height="100"
+        alt="News thubmnail"
+        className="m-3"
+      />
+      <a
+        href={link}
+        className="stretched-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      ></a>
+      {title}
+    </Container>
   );
 };
 

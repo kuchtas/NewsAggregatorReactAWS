aaf757e092e88c673a714aae7f58b73434d15ea7
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-07-30 14:27:10 +0200

    Add invoking of wprost, dziennik and oko lambdas

--- aws/parentLambda/index.js
+++ aws/parentLambda/index.js
@@ -1,15 +1,45 @@
 const AWS = require("aws-sdk");
 
 exports.handler = async (event) => {
+  let allArticles = [];
+
   const params = {
-    FunctionName:
-      "arn:aws:lambda:eu-central-1:286935822615:function:wyszukiwarka-child-wprost",
+    FunctionName: "",
     InvocationType: "RequestResponse",
     LogType: "Tail",
     Payload: JSON.stringify({ searchString: "katowice" }), //TO DO searchString passed from client with API Gateway
   };
 
-  const lambdaResult = await new AWS.Lambda().invoke(params).promise();
-  const resultObject = JSON.parse(lambdaResult.Payload);
-  return resultObject;
+  params.FunctionName =
+    "arn:aws:lambda:eu-central-1:286935822615:function:wyszukiwarka-child-wprost";
+  const wprostLambdaResult = await new AWS.Lambda().invoke(params).promise();
+  const wprostArticles = JSON.parse(JSON.parse(wprostLambdaResult.Payload))
+    .body;
+
+  params.FunctionName =
+    "arn:aws:lambda:eu-central-1:286935822615:function:wyszukiwarka-child-dziennik";
+  const dziennikLambdaResult = await new AWS.Lambda().invoke(params).promise();
+  const dziennikArticles = JSON.parse(JSON.parse(dziennikLambdaResult.Payload))
+    .body;
+
+  params.FunctionName =
+    "arn:aws:lambda:eu-central-1:286935822615:function:wyszukiwarka-child-oko";
+  const okoLambdaResult = await new AWS.Lambda().invoke(params).promise();
+  const okoArticles = JSON.parse(JSON.parse(okoLambdaResult.Payload)).body;
+
+  allArticles = [...wprostArticles, ...dziennikArticles, ...okoArticles];
+  shuffle(allArticles);
+  return allArticles;
 };
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  array.sort(function (a, b) {
+    return new Date(b.date) - new Date(a.date);
+  });
+  return array;
+}
